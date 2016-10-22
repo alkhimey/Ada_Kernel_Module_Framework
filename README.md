ccc6ec75afceb8e9127e18f28f0cdaf8e2a91545
Artium <artium@nihamkin.com>
2016-10-23 02:16:23 +0300

    Added license and readme

---
+++ README.md
@@ -0,0 +1,39 @@
+Ada Linux Kernel Module Toolkit
+===============================
+
+This toolkit allow writing Linux kernel modules suing the Ada programming langauge.
+
+## Prerequirments
+
+I used the following versions of the tools:
+
+```
+$ uname -rpio
+4.4.0-43-generic x86_64 x86_64 GNU/Linux
+$ gnat --version
+GNAT GPL 2016 (20160515-49)
+Copyright (C) 1996-2016, Free Software Foundation, Inc.
+This is free software; see the source for copying conditions.
+There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+$ gprbuild --version
+GPRBUILD GPL 2016 (20160515) (x86_64-pc-linux-gnu)
+Copyright (C) 2004-2016, AdaCore
+This is free software; see the source for copying conditions.
+There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+## Building
+
+```make```
+
+## Running
+
+A file called ```hello.ko``` will be generated.
+
+It is possible to insert and remove it using the ```insmod``` and ``rmmod``` commands in ```sudo``` mode.
+
+Use ```dmesg tail``` to see the output log. 
+
+## GNUv3 License
+
+See _COPYING3_ file for details.
--- rts/adainclude/last_chance_handler.adb
+++ rts/adainclude/last_chance_handler.adb
@@ -1,10 +1,38 @@
---                              -*- Mode: Ada -*-
---  Filename        : last_chance_handler.adb
---  Description     : Implementation of the exception handler for the kernel.
---  Author          : Luke A. Guest
---  Created On      : Thu Jun 14 12:06:48 2012
---  Licence         : See LICENCE in the root directory.
-
+------------------------------------------------------------------------------
+--                                                                          --
+--                        GNAT RUN-TIME COMPONENTS                          --
+--                                                                          --
+--                           LAST CHNACE HANDLER                            --
+--                                                                          --
+--                                 B o d y                                  --
+--                       (GNU-Linux/x86-64 Version)                         --
+--                                                                          --
+--          Copyright (C) 1992-2016, Free Software Foundation, Inc.         --
+--                                                                          --
+-- This specification is derived from the Ada Reference Manual for use with --
+-- GNAT. The copyright notice above, and the license provisions that follow --
+-- apply solely to the  contents of the part following the private keyword. --
+--                                                                          --
+-- GNAT is free software;  you can  redistribute it  and/or modify it under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  GNAT is distributed in the hope that it will be useful, but WITH- --
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.                                     --
+--                                                                          --
+--                                                                          --
+--                                                                          --
+--                                                                          --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+-- GNAT was originally developed  by the GNAT team at  New York University. --
+-- Extensive contributions were provided by Ada Core Technologies Inc.      --
+--                                                                          --
+------------------------------------------------------------------------------
 procedure Last_Chance_Handler
    (Source_Location : System.Address;
     Line            : Integer) is
