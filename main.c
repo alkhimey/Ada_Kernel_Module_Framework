d92168190846d3a0e7fa9930984fadee83d46668
Artium Nihamkin <artium@nihamkin.com>
2017-11-10 15:13:13 +0200

    Trying to do exception last chance handing using kthread wrapper

--- main.c
+++ main.c
@@ -1,22 +1,46 @@
 #include <linux/module.h> /* Needed by all modules */
 #include <linux/slab.h>
+#include <linux/kthread.h>
 
 extern void adakernelmoduleinit (void);
 //extern void adakernelmodulefinal (void);
 
 extern void ada_foo(void);
 
+static struct task_struct *task;
+
+
+/**
+ * Ada's part is wrapped in a kernel thread.
+ * If an exception is raised and not handled,
+ * the last chance handler will terminate the 
+ * container thread.
+ * 
+ */
+int init_thread(void *data)
+{
+    printk(KERN_ERR "Hello Ada.\n");
+    adakernelmoduleinit();
+    ada_foo();
+    printk(KERN_ERR "%s\n", "After Ada");
+    return 0; // ?
+}
 
 
 int init_module(void)
 {
+    int err;
+    printk(KERN_ERR "Init module.\n");    
 
-    kmalloc(1,1);
-    adakernelmoduleinit();
-    printk(KERN_ERR "Hello Ada.\n");
-    ada_foo();
-    printk(KERN_ERR "%s\n", "After Ada");
-    
+    task = kthread_run(init_thread, (void *)NULL, "init_thread");
+ 
+    if(IS_ERR(task)){ 
+        printk("Unable to start kernel thread.\n");
+        return PTR_ERR(task);
+    }
+//    return 0;
+    err = kthread_stop(task);
+    printk("%d\n", err);
     return 0;
 }
 
